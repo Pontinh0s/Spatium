ddbd7d935f2c156074274795bf02981bf80aaacf
Davidepaalte <davidepaalte@hotmail.com>
2014-01-14 17:11:42 +0000

    Este é o projeto do jogo.

---
+++ ComputacaoMovel.SpaceShip/src/com/example/computacaomovel/spaceship/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.computacaomovel.spaceship;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends SimpleBaseGameActivity {
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
