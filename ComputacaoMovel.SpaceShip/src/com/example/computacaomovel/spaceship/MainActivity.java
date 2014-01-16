b856779f2e85a19cdac9c15bb6b406527cc64c39
Davide Texiera <davidepaalte@hotmail.com>
2014-01-16 17:39:43 +0000

    Update MainActivity.java

--- ComputacaoMovel.SpaceShip/src/com/example/computacaomovel/spaceship/MainActivity.java
+++ ComputacaoMovel.SpaceShip/src/com/example/computacaomovel/spaceship/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.computacaomovel.spaceship;
 
+import java.io.IOException;
 import java.util.Random;
 
+import org.andengine.audio.music.Music;
+import org.andengine.audio.music.MusicFactory;
+import org.andengine.audio.sound.SoundFactory;
 import org.andengine.engine.Engine;
 import org.andengine.engine.FixedStepEngine;
 import org.andengine.engine.camera.Camera;
@@ -13,8 +17,10 @@ import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.engine.options.resolutionpolicy.FixedResolutionPolicy;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
@@ -23,8 +29,9 @@ import org.andengine.util.color.Color;
 import android.graphics.Rect;
 import android.view.MotionEvent;
 
-public class MainActivity extends BaseGameActivity {
+public class MainActivity extends BaseGameActivity implements IOnSceneTouchListener{
 	
+	Music mMusic;
 		
 	Scene cena = new Scene();
 	Camera mCamera = new Camera(0,0,CAMERA_WIDTH, CAMERA_HEIGHT);
@@ -39,29 +46,13 @@ public class MainActivity extends BaseGameActivity {
 		//eo = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);		
 		eo = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), mCamera);		
 		eo.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+		eo.getAudioOptions().setNeedsSound(true);
 		return eo;
 	}
 	
 	@Override
 	public Engine onCreateEngine(EngineOptions pEngineOptions) {
 		return new FixedStepEngine(eo, 200);
-	}
-	
-	public boolean onTouchEvent (MotionEvent event){
-		int myEventAction = event.getAction();
-		
-		float x = event.getX();
-		float y = event.getY();
-		
-		switch (myEventAction) {
-			case MotionEvent.ACTION_DOWN:
-				break;
-			case MotionEvent.ACTION_MOVE:
-				break;
-			case MotionEvent.ACTION_UP:
-				break;
-		}
-		return true;
 	}
 
 	@Override
@@ -73,7 +64,20 @@ public class MainActivity extends BaseGameActivity {
 		
 		
 		// -- FIM MENU --
-		
+
+		// ---------------Sounds---------------
+	    SoundFactory.setAssetBasePath("sounds/");
+	    MusicFactory.setAssetBasePath("sounds/");
+	    
+		// Load our "music.mp3" file into a music object
+	    try {
+	    	mMusic = MusicFactory.createMusicFromAsset(getMusicManager(), this, "background.ogg");
+		    mMusic.setLooping(true);
+	    } catch (IOException e) {
+	    	e.printStackTrace();
+	    }
+		// -------------Sounds_END-------------
+	    
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
 	}
 
@@ -81,6 +85,8 @@ public class MainActivity extends BaseGameActivity {
 	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
 			throws Exception {
 		
+
+		cena.setOnSceneTouchListener(this);
 		cena.registerUpdateHandler(new IUpdateHandler() {
 			
 			@Override
@@ -117,5 +123,13 @@ public class MainActivity extends BaseGameActivity {
 		B = B + 0.0005f*b;
 		
 		cena.setBackground(new Background(new Color(R,G,B)));
+	}
+
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		if (pSceneTouchEvent.isActionDown())
+			mMusic.play();
+		
+		return false;
 	}
 }
