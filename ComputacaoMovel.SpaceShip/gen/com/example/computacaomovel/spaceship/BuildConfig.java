fd3725ba0b212f9f0612431935b565598c8a9ef6
Davide Texiera <davidepaalte@hotmail.com>
2014-10-21 10:02:34 +0100

    deleted everything

--- ComputacaoMovel.SpaceShip/gen/com/example/computacaomovel/spaceship/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.computacaomovel.spaceship;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
